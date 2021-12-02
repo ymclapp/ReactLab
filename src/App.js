1a9ef7a31b9f090e8e880eddbd6d63191ecfd95f
ymcla <ymclapp@yahoo.com>
2021-12-02 15:22:27 -0600

    moved eslinter out of hooks folder and added auth back into app.js.  Error  was that index.js was not created yet.  Index is created and app is now working

--- src/App.js
+++ src/App.js
@@ -56,9 +56,9 @@ function App() {
           <Route path="/" exact>
             <ToDos message="Welcome!" nonadmin />
             <Users users={users} title="Users" />
-            {/*<Auth>*/}
-            <AddToDo onSubmit={submit} />
-            {/*</Auth>*/}
+            <Auth>
+              <AddToDo onSubmit={submit} />
+            </Auth>
           </Route>
 
           <Route path="/ToDos">
